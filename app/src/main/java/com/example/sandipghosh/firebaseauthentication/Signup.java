6454dac9b1cd7be5a0f974eff0619c8bfc40d6ea
sandipghosh <sandipgh19@gmail.com>
2017-05-28 22:59:02 +0530

    Signup function

--- app/src/main/java/com/example/sandipghosh/firebaseauthentication/Signup.java
+++ app/src/main/java/com/example/sandipghosh/firebaseauthentication/Signup.java
@@ -2,9 +2,19 @@ package com.example.sandipghosh.firebaseauthentication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by sandipghosh on 22/04/17.
@@ -13,11 +23,16 @@ import android.widget.ImageView;
 public class Signup extends AppCompatActivity {
 
     ImageView sback;
+    EditText inputEmail,inputPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         sback = (ImageView)findViewById(R.id.sback);
+        inputEmail = (EditText) findViewById(R.id.mail);
+        inputPassword = (EditText) findViewById(R.id.pswrd);
         sback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
@@ -25,7 +40,48 @@ public class Signup extends AppCompatActivity {
                 Intent it = new Intent(Signup.this, Splash.class);
                 startActivity(it);
 
+
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                //create user
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Signup.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(Signup.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(Signup.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(Signup.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+
             }
         });
+
     }
 }
