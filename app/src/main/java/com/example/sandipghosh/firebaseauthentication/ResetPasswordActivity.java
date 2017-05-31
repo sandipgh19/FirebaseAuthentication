9b08c668ad478cf07aa20007c60b8c65a8fc51b5
sandipghosh <sandipgh19@gmail.com>
2017-05-31 20:22:25 +0530

    Update Signup

---
+++ app/src/main/java/com/example/sandipghosh/firebaseauthentication/ResetPasswordActivity.java
@@ -0,0 +1,8 @@
+package com.example.sandipghosh.firebaseauthentication;
+
+/**
+ * Created by sandipghosh on 31/05/17.
+ */
+
+public class ResetPasswordActivity {
+}
--- app/src/main/java/com/example/sandipghosh/firebaseauthentication/Signup.java
+++ app/src/main/java/com/example/sandipghosh/firebaseauthentication/Signup.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -22,24 +23,41 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Signup extends AppCompatActivity {
 
-    ImageView sback;
-    EditText inputEmail,inputPassword;
+    private EditText inputEmail, inputPassword;
+    private Button btnSignIn, btnSignUp, btnResetPassword;
     private ProgressBar progressBar;
     private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
-        sback = (ImageView)findViewById(R.id.sback);
-        inputEmail = (EditText) findViewById(R.id.mail);
-        inputPassword = (EditText) findViewById(R.id.pswrd);
-        sback.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                Intent it = new Intent(Signup.this, Splash.class);
-                startActivity(it);
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
 
+        btnSignIn = (Button) findViewById(R.id.sign_in_button);
+        btnSignUp = (Button) findViewById(R.id.sign_up_button);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        btnResetPassword = (Button) findViewById(R.id.btn_reset_password);
+
+        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signup.this, ResetPasswordActivity.class));
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
                 String email = inputEmail.getText().toString().trim();
                 String password = inputPassword.getText().toString().trim();
@@ -82,6 +100,11 @@ public class Signup extends AppCompatActivity {
 
             }
         });
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
     }
 }
